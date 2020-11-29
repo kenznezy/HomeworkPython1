9975ec860883e315e2232b5461ab9a7bd9bfddfe
kenznezy <hotdog4ik@gmail.com>
2020-11-29 16:55:01 +0300

    Added HW 2 and 3, 2 is WIP

---
+++ HW3.py
@@ -0,0 +1,14 @@
+# 4. Найти значение минимального элемента списка
+
+import random
+
+n = int(input("Input list size:\n"))
+
+A = []
+for i in range(n):
+    A.append(random.randint(0,99))
+
+print(A)
+B=set(A)
+A=list(B)
+print(A[0])
